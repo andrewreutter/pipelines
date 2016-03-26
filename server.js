16645eda14514244ee8f915e1c3311d73c122bd8
ANDREW REUTTER <andrewreutter@ANDREWs-MacBook.local>
2016-03-26 18:05:01 -0500

    multiport

--- server.js
+++ server.js
@@ -3,19 +3,31 @@
     var express = require('express'),
         bodyParser = require('body-parser'),
 
-        pipelines = require('./config/pipelines')
-        ;
+        pipelines = require('./config/pipelines'),
 
-    var app = express();
-    app.use(bodyParser.json());
+        FIRST_PORT = 3000,
+        current_port = FIRST_PORT,
+        next_port = function() { return current_port++; }
+    ;
+
 
     pipelines.AS_ARRAY.forEach(function(pipeline) {
-        var pipeline_url = '/pipeline/' + pipeline.name;
-        console.info('serving pipeline at ' + pipeline_url);
+        var app = express(),
+            pipeline_url = '/pipeline/' + pipeline.name,
+            pipeline_port = next_port()
+            ;
 
-        //pipeline.inputs().forEach(function(input) {
-        //    console.log('XXXInput', input)
-        //});
+        app.use(bodyParser.json());
+
+        pipeline.inputs().forEach(function(input) {
+            console.info('\tinput', input)
+        });
+        pipeline.converts().forEach(function(input) {
+            console.info('\tconvert', input)
+        });
+        pipeline.outputs().forEach(function(input) {
+            console.info('\toutput', input)
+        });
 
         app.get(pipeline_url, function (req, res) {
             res.send('Hello ' + pipeline.name + '!')
@@ -23,13 +35,10 @@
         app.post(pipeline_url, function (req, res) {
             res.send(pipeline.execute(req.body));
         });
+
+        app.listen(pipeline_port, function () {
+            console.info('Serving pipeline at ' + pipeline_url + ':' + pipeline_port);
+        });
     });
 
-    app.get('/', function (req, res) {
-        res.send('Hello World!');
-    });
-
-    app.listen(3000, function () {
-        console.log('Example app listening on port 3000!');
-    });
 })();
