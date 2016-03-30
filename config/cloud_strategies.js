4663e339ba458ce6dd6ab7a0aebb1929da82abbe
andrew.reutter@gmail.com <andrewreutter@ANDREWs-MacBook.local>
2016-03-29 23:01:22 -0500

    cloudable.js and tests

--- config/cloud_strategies.js
+++ config/cloud_strategies.js
@@ -8,15 +8,15 @@ var cloud_strategy = require('../lib/cloud_strategy');
             make_cloud_store:                       cloud_strategy.LOCAL.make_cloud_store,
             make_cloud_store_trigger_for_function:  cloud_strategy.LOCAL.make_cloud_store_trigger_for_function,
             make_post_trigger_for_function:         cloud_strategy.LOCAL.make_post_trigger_for_function
-        }),
-
-        cloud_strategy('aws_lambda', {
-            // TODO: implement me!
-        }),
-
-        cloud_strategy('google_cloud_functions', {
-            // TODO: implement me!
         })
+
+        //cloud_strategy('aws_lambda', {
+        //    // TODO: implement me!
+        //})
+        //
+        //cloud_strategy('google_cloud_functions', {
+        //    // TODO: implement me!
+        //})
     ];
 
     module.exports = {
